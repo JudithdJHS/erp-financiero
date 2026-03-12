4bef9657aca4758306d885876f0be162e4051be9
JudithdJHS <132173175+JudithdJHS@users.noreply.github.com>
2026-03-12 16:02:14 +0100

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,12 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from app.config import settings
+from config import settings
 
-# Añadimos pool_pre_ping para asegurar que la conexión esté viva siempre
-engine = create_engine(
-    settings.DATABASE_URL, 
-    pool_pre_ping=True,
-    connect_args={"sslmode": "require"} # Requerido por Render/PostgreSQL
-)
+# Conexión segura para Render
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
