1ac3ac4644df6660bbeef1657c6486e63283b3a4
JudithdJHS <132173175+JudithdJHS@users.noreply.github.com>
2026-03-12 18:14:00 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Settings(BaseSettings):
           SECRET_KEY: str = "9261aa73-00e0-4899-a451-0713ad90a8b9"
           ACCESS_TOKEN_EXPIRE_MINUTES: int = 10080
           class Config: env_file = ".env"
-    settings = Settings()
+settings = Settings()
