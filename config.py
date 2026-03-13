884ffdc7c0c02a93f2fcd37f3eb57e9b887ebaff
JudithdJHS <132173175+JudithdJHS@users.noreply.github.com>
2026-03-13 09:05:59 +0100

    Update backend config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = ""
+    DATABASE_URL: str = "postgresql://erp_user:erp_password@db:5432/erp_financiero"
     GCP_PROJECT_ID: str = ""
     GCP_LOCATION: str = "us"
     GCP_PROCESSOR_ID: str = ""
-    SECRET_KEY: str = "9261aa73-00e0-4899-a451-0713ad90a8b9"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 10080
+    SECRET_KEY: str = "your-super-secret-key-change-in-prod"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7 # 1 semana
 
     class Config:
         env_file = ".env"
